e8506434b3a376a023bcff6411ddceca3ea295f7
Dhirendra Singh <dhi.singh@gmail.com>
2017-12-15 14:26:38 +1100

    Create base dir explicitly first

--- run.sh
+++ run.sh
@@ -6,10 +6,15 @@ DIR=$(dirname "$0")
 # will save the tzar release to this file
 JAR=$DIR/tzar.jar
 
+# will run the modelcode here
+TZAR_BASEDIR=/tmp/tzar
 # get it if not there already
 if [ ! -f $JAR ]; then
   wget -O $JAR https://github.com/agentsoz/tzar-framework/raw/dev/releases/tzar.20171215.jar
 fi
+
+# create the tmp base dir
+rm -rf $TZAR_BASEDIR && mkdir $TZAR_BASEDIR
 
 # now run it
 java -jar $JAR execlocalruns https://github.com/ascelin/tzar-example-R-pop-model.git --tzarbasedir /tmp/tzar --repotype=GIT
